3bd2d7c6fc6c72abd370caadad91291e2db9bfd7
Connor Kalinowski <02connor.kalinowski@gulllakecs.org>
2019-03-13 10:02:36 -0700

    Paddles full working

--- Coding/Final Exam Code.py
+++ Coding/Final Exam Code.py
@@ -1,6 +1,10 @@
 #Simple pong game
 #3.12.19
 # Connor
+'''
+This is my simple Pong game that will be a multiplayer game with a workign score and sound effects.
+Credit: @TokyoEdTech
+'''
 
 import turtle
 
@@ -12,20 +16,33 @@ wn.tracer(0)
 
 
 # Main-game-loop
-while True:
-	wn.update()
 # Paddle-A
 
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
 
 
-# Paddle B
+
+
+# Paddle-b
+
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(350, 0)
+
+
+while True:
+	wn.update()
 
 
 
